453418fec556d97856454522715e388033309568
manwlis45 <57815783+manwlis45@users.noreply.github.com>
2019-11-16 20:57:48 +0200

    Update email_preprocess.py

--- tools/email_preprocess.py
+++ tools/email_preprocess.py
@@ -33,7 +33,7 @@ def preprocess(words_file = "../tools/word_data.pkl", authors_file="../tools/ema
     authors = pickle.load(authors_file_handler)
     authors_file_handler.close()
 
-    words_file_handler = open(words_file, "r")
+    words_file_handler = open(words_file, "rb")
     word_data = pickle.load(words_file_handler)
     words_file_handler.close()
 
